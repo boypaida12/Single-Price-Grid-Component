c266761147f50eaeb20c0d3831fe087e78a09047
boypaida12 <baddysackey5@gmail.com>
2023-01-16 00:53:06 +0000

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./build/*.html'],
+  theme: {
+    fontFamily: {
+      'sans': ["Karla, sans-serif"]
+    },
+    extend: {
+      colors: {
+        'cyan': {
+          55: 'hsl(179, 62%, 43%)',
+        },
+        'yellow': {
+          450: 'hsl(71, 73%, 54%)'
+        },
+        'blue-grey': {
+          50: 'hsl(218, 22%, 67%)'
+        },
+        'light-grey': {
+          50: 'hsl(204, 43%, 93%)'
+        },
+      }
+    },
+  },
+  plugins: [],
+}
